0e892bbaa2cba2591ca7caec3c67809e8570351d
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2024-01-12 10:40:16 +0900

    :bug: X-FORWARDED-FOR header parsing for daily visitor count

--- src/main/java/com/example/userservice/global/helper/IpHelper.java
+++ src/main/java/com/example/userservice/global/helper/IpHelper.java
@@ -1,33 +1,35 @@
 package com.example.userservice.global.helper;
 
-import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+
+@Slf4j
 public class IpHelper {
 
 
     public static String getClientIpAddr(HttpServletRequest request) {
-        String ip =null;
 
         String clientIpAddr = request.getHeader("X-FORWARDED-FOR");
 
-        if (clientIpAddr == null || clientIpAddr.isEmpty()) {
-            // If the header is not present, fall back to the default way of obtaining the client's IP address
-            ip = request.getRemoteAddr();
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("WL-Proxy-Client-IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_CLIENT_IP");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            ip = request.getRemoteAddr();
+        if (clientIpAddr != null && !clientIpAddr.isEmpty()) {
+            String[] ipAddresses = clientIpAddr.split(",");
+            clientIpAddr = ipAddresses[0].trim();
         }
 
-        return ip;
+        Enumeration<String> headerNames = request.getHeaderNames();
+
+        if (headerNames != null) {
+            while (headerNames.hasMoreElements()) {
+                String name = headerNames.nextElement();
+                String value = request.getHeader(name);
+                log.info("Header ::: " + name + " : " + value);
+            }
+        }
+
+        return clientIpAddr;
     }
 
 }
