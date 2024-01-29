d3999de2076d59d557dbe449aa06fc6a178514a6
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2024-01-29 21:20:08 +0900

    :sparkles: update pom.xml

--- src/test/java/com/example/userservice/UserServiceApplicationTests.java
+++ src/test/java/com/example/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.example.userservice;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class UserServiceApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.example.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
